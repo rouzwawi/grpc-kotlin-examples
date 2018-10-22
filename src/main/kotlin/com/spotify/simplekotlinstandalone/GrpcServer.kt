025b5509ae47dca97a6ff467ff65de7dca3a8f20
Emilio Del Tessandoro <emilio@spotify.com>
2018-10-22 17:07:10 +0200

    initial commit

---
+++ src/main/kotlin/com/spotify/simplekotlinstandalone/GrpcServer.kt
@@ -0,0 +1,21 @@
+package com.spotify.simplekotlinstandalone
+
+import io.grpc.ManagedChannelBuilder
+import io.grpc.ServerBuilder
+//import services.KeyValueServiceGrpc
+
+fun main(args: Array<String>) {
+
+    val channel = ManagedChannelBuilder.forAddress("localhost", 15000).usePlaintext(true).build()
+//    val kvsClient = KeyValueServiceGrpc.newBlockingStub(channel)
+
+//    val server = ServerBuilder.forPort(15001).addService(UserService(kvsClient)).build()
+//    server.start()
+
+//    println("User service started")
+//
+//    Runtime.getRuntime().addShutdownHook(Thread() { println("Ups, JVM shutdown") })
+//    server.awaitTermination()
+//
+//    println("User service stopped")
+}
---
+++ src/main/kotlin/com/spotify/simplekotlinstandalone/UserService.kt
@@ -0,0 +1,37 @@
+package com.spotify.simplekotlinstandalone
+
+import com.google.common.base.Preconditions
+import io.grpc.stub.StreamObserver
+//import services.GetRequest
+//import services.KeyValueServiceGrpc.KeyValueServiceBlockingStub
+import services.UserRequest
+import services.UserResponse
+import services.UserServiceGrpc.UserServiceImplBase
+
+//class UserService(keyValue: KeyValueServiceBlockingStub) : UserServiceImplBase() {
+
+class UserService() : UserServiceImplBase() {
+//    private val keyValue = Preconditions.checkNotNull(keyValue)
+//
+//    override fun getUser(request: UserRequest, responseObserver: StreamObserver<UserResponse>) {
+
+//        fun getValue(key: String) = keyValue.get(
+//                GetRequest
+//                        .newBuilder()
+//                        .setKey(request.name + key)
+//                        .build() ?: throw IllegalArgumentException("key not found")
+//        ).value
+
+//        val response = UserResponse
+//                .newBuilder()
+//                .setName(request?.name ?: throw IllegalArgumentException("name can not be null"))
+//                .setEmailAddress(getValue(".email"))
+//                .setCountry(getValue(".country"))
+//                .setActive(getValue(".active").toBoolean())
+//                .build()
+//
+//        responseObserver.onNext(response)
+//        responseObserver.onCompleted()
+//    }
+
+}
